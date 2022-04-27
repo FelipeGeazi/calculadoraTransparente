d745583775e3adb63b6f0d4e2cada9f70844ab0c
FelipeGeazi <felipegeaziartes@gmail.com>
2022-04-27 13:49:26 -0300

    excluindo

--- calculadoraTransparente.js
+++ calculadoraTransparente.js
@@ -2,10 +2,3 @@
 //A função além de trazer o resultado. 
 // também colocará as informções do input1 para o input2
 // Na sequencia ativar o alerta
-
-VanillaTilt.init(document.querySelector(".container"), {
-    max: 25,
-    speed: 400,
-    glare: true,
-    "max-glare": 0.2,
-});
