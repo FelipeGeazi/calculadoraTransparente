abfa25f7979a969a1df677191f03a30b1be872a0
FelipeGeazi <felipegeaziartes@gmail.com>
2022-04-27 06:37:29 -0300

    primeiro commit

---
+++ calculadoraTransparente.js
@@ -0,0 +1,12 @@
+// Após apertar o sinal de igual.Ativar a função 
+//A função além de trazer o resultado. 
+// também colocará as informções do input1 para o input2
+// Na sequencia ativar o alerta
+
+
+VanillaTilt.init(document.querySelector(".container"), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.2,
+});
